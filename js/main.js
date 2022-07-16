de5cca7d975bc1b989a628513447f598a9a4f930
Gilberto29ap <gilberto29ap@gmail.com>
2022-07-16 16:12:13 -0300

    Criação de css e js

---
+++ js/main.js
@@ -0,0 +1,57 @@
+var num = 3
+var num2 = 5
+
+
+
+function clique2(){
+    return clique()
+}
+function redirecionar(){
+    window.open("https://www.google.com")
+}
+
+
+
+/*
+var lista = [{fruta: "maca", cor: "vermelha"}]
+alert("Vamos criar uma lista de frutas?")
+var tamanho = prompt("Quantas frutas?")
+for (tamanho; tamanho > 0; tamanho--){
+    lista[0].push(fruta: prompt("Digite o nome da fruta"))
+    lista[0].push(cor: "da")
+}
+
+
+alert("A lista criada é: " + lista.join(", "))
+*/
+/*
+function idadeprev(idade, anoprev){
+    var anohj = new Date
+    var idprev = anoprev - anohj.getFullYear() + idade
+
+    return idprev
+}
+
+var id = prompt ("Qual sua idade?")
+var ano = prompt ("Qual o ano de previsão?")
+
+alert(`Sua idade no ano de ${ano} será:` + idadeprev(id,ano))
+*/
+
+
+/*
+var nome = "Gilberto Cruz"
+var idade = prompt("Olá, qual sua idade?")
+
+while(idade<35)   {
+    alert(`Sua idade é ${idade}`)
+    idade ++
+}
+
+*/
+
+
+/*
+if(idade>=18){alert(`Bem vindo ${nome} você é maior de idade`)}
+else{alert(`${nome}, este site é para maiores de idade`)}
+*/
